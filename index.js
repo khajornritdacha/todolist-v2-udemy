7d40483eb9cab13676d6a33d8d4bf42471e0a0ae
Joo <vijak13019@hotmail.com>
2022-12-08 19:55:14 +0700

    fix(index): edit static path

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static("public/"));
 
 const PORT = process.env.PORT || 8080;
 const DB_URL = process.env.DB_URL;
